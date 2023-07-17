6e71689433592830cff7e390ee37926b83f77d12
TJ Stifter <tjstifter@gmail.com>
2023-07-17 10:14:30 -0400

    empty database, alembic migrations initiated

---
+++ lib/db/migrations/versions/09e55e99c20a_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 09e55e99c20a
+Revises: 
+Create Date: 2023-07-17 10:03:29.586878
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '09e55e99c20a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
