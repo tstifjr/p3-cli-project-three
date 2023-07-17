9942a22ee740356c6699d94d801c9cf6721aa11f
TJ Stifter <tjstifter@gmail.com>
2023-07-17 15:59:25 -0400

    adds musicians and instruments table

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy import String, Integer, Column, Boolean, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -7,3 +8,21 @@ Session = sessionmaker( bind = engine )
 session = Session()
 
 Base = declarative_base()
+
+
+class Musician (Base):
+    __tablename__ = 'musicians'
+
+    id = Column(Integer(), primary_key = True)
+    name = Column(String())
+    age = Column(Integer())
+    email = Column(String())
+    skill_level = Column(Integer())
+    location = Column(String())
+    instrument_id = Column(Integer(), ForeignKey('instruments.id'))
+
+class Instrument (Base):
+    __tablename__ = 'instruments'
+
+    id = Column(Integer(), primary_key= True)
+    name = Column(String())
